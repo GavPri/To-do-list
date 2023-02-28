9e3d08b9d3d4d803c5e16bbcacd9eca5151c66b1
GavPri <gavinprior5@gmail.com>
2023-02-28 19:48:22 +0000

    remove imported java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,11 @@ let dateString = today.toLocaleDateString('en-US', display);
 
 let header = document.getElementById('date-header');
 header.textContent = `${dateString}`;
+
+// auto type 
+let typed = new typed('.auto-text',{
+    strings: ['Get Productive','Reduce Stress','Manage Time','Collaborate Better','Take Control'],
+    typedSpeed:150,
+    backSpeed:150,
+    loop:true
+});
